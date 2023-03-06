11a6b8f7ad437f4505fa5383847b8962551b3862
ntelis9 <nntelakis@gmail.com>
2023-03-06 16:49:42 +0200

    Hello World v 0.2

--- src/gr/aueb/elearn/ch1/HelloWorld.java
+++ src/gr/aueb/elearn/ch1/HelloWorld.java
@@ -1,5 +1,10 @@
 package gr.aueb.elearn.ch1;
 
+/**
+ * Prints Hello World!
+ * @author N. Ntelakis
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
